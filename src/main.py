2131bf8d0403952e2e818616a48f8cc1988b57f0
prosti221 <faisalx12@gmail.com>
2023-05-07 12:10:47 +0200

    Added depth estimation for each car based on the depth map and bounding box

--- src/main.py
+++ src/main.py
@@ -1,18 +1,105 @@
 import cv2
 import numpy as np
-from utils import *
 from scipy.stats import iqr
 import matplotlib.pyplot as plt
+import torch
 
-from videoProcessor import VideoProcessor
 from depthEstimation import DepthEstimator
+from vehicleDetection import VehicleDetector
+
+def drawDetections(img, detections):
+    # detections is a pandas dataframe with columns: xmin, ymin, xmax, ymax, confidence, class, name
+    detection_img = img.copy()
+    # Draw detections and add depth values as a label to the rectangle
+    for index, row in detections.iterrows():
+        xmin, ymin, xmax, ymax = row['xmin'], row['ymin'], row['xmax'], row['ymax']
+        # Round xmin, ymin, xmax, ymax to int
+        xmin, ymin, xmax, ymax = int(xmin), int(ymin), int(xmax), int(ymax)
+        cv2.rectangle(detection_img, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+        depth_label = "Depth: {:.2f} m".format(round(row['depth'], 2))
+        # Add black background panel to depth label
+        label_size, _ = cv2.getTextSize(depth_label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 2)
+        panel_size = (label_size[0]+10, label_size[1]+10)
+        panel_pos = (xmin, ymin - panel_size[1])
+        cv2.rectangle(detection_img, panel_pos, (panel_pos[0]+panel_size[0], panel_pos[1]+panel_size[1]), (0,0,0), -1)
+        cv2.putText(detection_img, depth_label, (panel_pos[0]+5, panel_pos[1]+label_size[1]+5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+    return detection_img
+
+def getMeanDepth(depth, detections):
+    # detections is a pandas dataframe with columns: xmin, ymin, xmax, ymax, confidence, class, name
+    # Get depth of each detection
+    # Add depth to detection data frame as a new column
+    depth_per_detection = []
+    for i, detection in detections.iterrows():
+        xmin, ymin, xmax, ymax = detection["xmin"], detection["ymin"], detection["xmax"], detection["ymax"]
+        # Round xmin, ymin, xmax, ymax to int
+        xmin, ymin, xmax, ymax = int(xmin), int(ymin), int(xmax), int(ymax)
+        depth_values = depth[ymin:ymax, xmin:xmax].flatten()
+        depth_values = depth_values[depth_values != 0] # remove zeros
+        depth_per_detection.append(depth_values.mean())
+
+    # Add depth to detection data frame as a new column
+    detections["depth"] = depth_per_detection
+
+    return detections
+
+
 
 if __name__ == "__main__":
-    frame_rate = 1 
+    frame_rate = 5 
 
-    # Convert video to frames, and estimate depth map per frame
-    video_processor = VideoProcessor(video_path='./videos/dji_vid4.mp4', frames_path='./frames', frame_rate=frame_rate)
-    # Shows the frame and depth map side by side
-    video_processor.show_split_frames() 
+    # Dummy camera intrinsic parameters
+    image_width = 1280
+    image_height = 720
 
+    font                   = cv2.FONT_HERSHEY_SIMPLEX
+    bottomLeftCornerOfText = (10,20)
+    fontScale              = 1
+    fontColor              = (0,0,0)
+    thickness              = 2 
+    lineType               = 2
+
+    #cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture('test_images/vid1.mp4')
+    cap.set(cv2.CAP_PROP_FPS, frame_rate)
+    follower_depth_estimator = DepthEstimator(model_type='DPT_Large') # DPT_Large, MiDaS_small DPT_Large is more accurate but slower. MiDaS_small seems to be good enough though.
+    vehicle_detector = VehicleDetector('./yolov5.pt', device='cpu')
+    initial_frame = np.zeros((1280, 720, 3), dtype=np.uint8)
+
+    while True:
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+        ret, frame = cap.read()
+
+        # Predict depth
+        follower_depth_estimator.load_images_from_array([frame]) # Load first frame for testing
+        depth_map = follower_depth_estimator.predict_depth()
+
+        depth_map_norm = cv2.normalize(depth_map[0], None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        colormap_depth_map = cv2.applyColorMap(depth_map_norm, cv2.COLORMAP_JET)
+
+        # Detect vehicles
+        vehicle_boxes = vehicle_detector.detect(frame)
+        vehicle_boxes = getMeanDepth(depth_map[0], vehicle_boxes)
+
+        detection_img = drawDetections(frame, vehicle_boxes)
+        #print(vehicle_boxes)
+        cv2.imshow('detection_img', detection_img)
+
+        #stack = np.hstack((frame, colormap_depth_map))
+        #cv2.imshow('Frame and corresponding depth map', stack)
+
+        baseline = 0
+
+        cv2.putText(frame,f'Distance from initial frame: {baseline}',
+            bottomLeftCornerOfText,
+            font,
+            fontScale,
+            fontColor,
+            thickness,
+            lineType)
+
+    cap.release()
+    cv2.destroyAllWindows()
 
--- src/vehicleDetection.py
+++ src/vehicleDetection.py
@@ -3,21 +3,25 @@ import matplotlib.pyplot as plt
 import cv2
 
 class VehicleDetector:
-    def __init__(self, weights_path, device):
+    def __init__(self, weights_path, device='cpu'):
         self.weights_path = weights_path
         self.model = torch.hub.load('ultralytics/yolov5', 'custom', path=weights_path).to(device)
+        self.current_prediction = None
 
     def detect(self, inp):
         pred = self.model(inp)
+        self.current_prediction = pred
         # Return detections in pandas dataframe containing:
         # [xmin, ymin, xmax, ymax, confidence, class, name]
-        return  pred.pandas().xyxy[0] 
+        return  pred.pandas().xyxy[0].sort_values(by=['confidence'], ascending=False)
 
-
+    def show_detections(self):
+        if self.current_prediction is not None:
+            self.current_prediction.show()
 
 # Loading in yolov5s - you can switch to larger models such as yolov5m or yolov5l, or smaller such as yolov5n
 if __name__ == '__main__':
-    detector = VehicleDetector('./yolov5.pt', 'cpu')
+    detector = VehicleDetector('./yolov5.pt', device='cpu')
 
     # Run inference
     img_path = './test_images/img5.png'
